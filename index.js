f25747e26cbe16b31d7fd5d7208383ce3d9442da
Lewis Nozaki <lhnozaki@gmail.com>
2019-07-18 19:16:20 -1000

    passed tests

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const emailLog = require('./email_log.json');
+const emailLog = require("./email_log.json");
 
 function countUniqueEmails(emailFile) {
   // do work here
+
+  let newObj = emailFile.emails.reduce(function(p, c) {
+    if (c.email in p) {
+      p[c.email]++;
+    } else {
+      p[c.email] = 1;
+    }
+    return p;
+  }, {});
+
+  return newObj;
 }
 
 console.log(countUniqueEmails(emailLog));
